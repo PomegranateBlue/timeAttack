d6aaf16fcbe6d3dc23a5d9fb18ee107a0c95ad5b
bluepome98 <wpdn1998@gmail.com>
2025-01-27 12:21:43 +0900

    1시 27분

---
+++ src/App.jsx
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [medalList, setMedalList] = useState([
+    { country: "대한민국", gold: 1, silver: 1, copper: 1 },
+  ]);
+  const [country, setCountry] = useState("");
+  const [goldMedal, setGoldMedal] = useState(0);
+  const [silverMedal, setSilverMedal] = useState(0);
+  const [copperMedal, setCopperMedal] = useState(0);
+
+  const addMedal = (e) => {
+    e.preventDefault();
+    const newMedal = {
+      country: country,
+      gold: goldMedal,
+      silver: silverMedal,
+      copper: copperMedal,
+    };
+    if (e.target.value === null) {
+      alert("입력 양식을 채워주세요");
+      return;
+    }
+    setMedalList([...medalList, newMedal]);
+    resetForm();
+  };
+  const updateMedal = () => {
+    const updateMedal = {
+      gold: goldMedal,
+      silver: silverMedal,
+      copper: copperMedal,
+    };
+    setMedalList([...medalList, updateMedal]);
+    resetForm();
+  };
+  const deleteMedal = (country) => {
+    setMedalList(medalList.filter((item) => item.country !== country));
+  };
+
+  const resetForm = () => {
+    setCountry("");
+    setGoldMedal(0);
+    setSilverMedal(0);
+    setCopperMedal(0);
+  };
+  return (
+    <div>
+      <h1>Olympic TimeAttack</h1>
+      <form className="medal-form">
+        <label>국가명</label>
+        <input
+          type="text"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+          required
+          min="0"
+          max="99"
+        />
+        <label>금메달</label>
+        <input
+          type="number"
+          value={goldMedal}
+          onChange={(e) => setGoldMedal(e.target.value)}
+          required
+          min="0"
+          max="99"
+        />
+        <label>은메달</label>
+        <input
+          type="number"
+          value={silverMedal}
+          onChange={(e) => setSilverMedal(e.target.value)}
+          required
+          min="0"
+          max="99"
+        />
+        <label>동메달</label>
+        <input
+          type="number"
+          value={copperMedal}
+          onChange={(e) => setCopperMedal(e.target.value)}
+          required
+          min="0"
+          max="99"
+        />
+        <div className="btn-container">
+          <button type="submit" onClick={addMedal}>
+            추가
+          </button>
+          <button type="button" onClick={() => updateMedal(medalList.country)}>
+            업데이트
+          </button>
+        </div>
+      </form>
+      <div className="medalList-container">
+        <ul>
+          {medalList.map((item) => (
+            <li key={item.country}>
+              <label></label>
+              {item.country}
+              <label>금메달</label>
+              {item.gold}
+              <label>은메달</label>
+              {item.silver}
+              <label>동메달</label>
+              {item.copper}
+              <button onClick={() => deleteMedal(item.country)}>삭제</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
